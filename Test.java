8d3abc0d200b811c6fa1dd14948bb8ecf0255629
VishnuKanna <vishnumas111@gmail.com>
2023-04-06 17:06:16 +0530

    Added Test.java

---
+++ Test.java
@@ -0,0 +1,15 @@
+
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a =2;
+		int b = 3;
+		
+		System.out.println(a + b);
+
+	}
+
+}
