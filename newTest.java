1df82c3dfa65d0998f81a61797bd373898a5e4af
VishnuKanna <vishnumas111@gmail.com>
2023-04-10 10:27:51 +0530

    Added 1 more file

---
+++ newTest.java
@@ -0,0 +1,16 @@
+
+public class Student {
+
+	private int id;
+
+	private String name;
+
+	private String phoneNumber;
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", phoneNumber=" + phoneNumber + "]";
+	}
+	
+	
+	
+}
